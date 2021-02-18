1a7a3ca780056d2df8a5e2873304071f0ec49c30
Ayaan Merchant <ayaan.merchant08@gmail.com>
2021-02-18 16:49:00 +0000

    Rebranded the entire site, deleted owl carousel, updated logo

--- js/main.js
+++ js/main.js
@@ -1,21 +1,14 @@
-// toggle
 $(document).ready(function(){
 
   $nav = $('.nav');
   $toggle = $('.toggle');
 
   $toggle.click(function(){
-    $nav.toggleClass('collapse')
+    $nav.toggleClass('collapse');
   })
-})
 
-// owl carousel
-$('.owl-carousel').owlCarousel({
-  loop: true,
-  autoplay: true,
-  autoplayTimeout: 4000,
-  dots: false,
 });
+
 
 
 
