7e304a0bd3cb55699c2903b91fc966b2f4bbaee0
Ayaan Merchant <ayaan.merchant08@gmail.com>
2021-02-26 00:53:51 +0000

    place holder text to footer stuffs. Made sources tab

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 $(document).ready(function(){
 
-  $nav = $('.nav');
-  $toggle = $('.toggle');
+  $(".toggle").on("click", function(){
+    $('.nav').toggleClass('collapse');
+  });
 
-  $toggle.click(function(){
-    $nav.toggleClass('collapse');
-  })
+  $(".source").on("click", function(){
+    $('.tab').toggleClass('display');
+  });
 
   // owl carousel initialization 
   $('.owl-carousel').owlCarousel({
     dots: false,
     nav: true,
     navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
-    items: 1,
-    responsive : {
+    items: 1, 
+    responsive: {
       0: {items: 1},
       420: {items: 1 }, 
       560: {items: 2 }, 
